7050db516e2f1066abb8fff800c038dd4e610f55
stefanstanimirovic <stanimirovic.stefan@gmail.com>
2022-12-08 19:03:51 +0100

    while petlja

---
+++ 11_WHILE/script.js
@@ -0,0 +1,27 @@
+// Ispis poruke "Zdravo" pet puta
+let i = 1;
+while(i <= 5)
+{
+    console.log("Zdravo");
+    i++;
+}
+
+console.log("Poruka nakon while petlje");
+console.log(`Vrednost promenljive i nakon while petlje je: ${i}`);
+
+// Ispis brojeva od 1 do 5
+i = 1;
+while(i <= 5)
+{
+    console.log(i);
+    i++;
+}
+
+/*
+
+if(uslov) { ... } 
+while(uslov) { ... }
+
+IF    ->  Blok naredbi se izvrsava 0 ili 1 puta
+WHILE ->  Blok naredbi se izvrsava 0, 1, 2, 3, 4, ........
+*/
