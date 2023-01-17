09502b93768b4ebcdc3b45f1e6861c04dd67495d
JelenaMatejic <jelena.jelena.matejic@gmail.com>
2023-01-17 17:23:57 +0100

    24_TO_DO_LISTA_Verzija2

--- 24_TO_DO_LISTA/Verzija2/script.js
+++ 24_TO_DO_LISTA/Verzija2/script.js
@@ -9,7 +9,27 @@ btnDodaj.addEventListener("click", () => {
     if(inputTaskValue != "") {
         let liNewTask = document.createElement("li"); // Kreiram novi <li>
         liNewTask.textContent += inputTaskValue; // Upisujem u <li> tekst
-        ulTasks.appendChild(liNewTask); // Dodajemo <li> čvor na kraj <ul>
+        
+        let radioAdd = document.querySelector("input[name=add]:checked");
+        // console.log(radioAdd, radioAdd.value);
+        if(radioAdd.value == 'beggin') {
+            ulTasks.prepend(liNewTask); // Dodajemo <li> čvor na početak <ul>
+        }
+        else {
+            ulTasks.appendChild(liNewTask); // Dodajemo <li> čvor na kraj <ul>
+        }
         inputTask.value = ""; /// Isprazni input polje nakon dodavanja elementa u <ul>
     }
 })
+
+ulTasks.addEventListener("click", e => {
+    // console.log("Kliknuto na UL");
+    // console.log(e);
+    // console.log(e.target);
+    // console.log(e.target.tagName);
+    if(e.target.tagName == "LI") {
+        // console.log("Kliknuto na LI");
+        // e.target.classList.toggle("precrtaj");
+        e.target.remove();
+    }
+});
